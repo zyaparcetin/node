1bd3578655cc1402e9169c765cbd484ff03aefa8
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-02-23 10:52:11 +0100

    Add some variables in app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-// CommonJS, every file is module (by default)
-// Modules - Encapsulated Code (only share minimum)
-const names = require('./4-names')
-const sayHi = require('./5-utils')
-const data = require('./6-alternative-flavor')
-require('./7-mind-grenade')
-sayHi('susan')
-sayHi(names.john)
-sayHi(names.peter)
-//add something later
+const path = require('path')
+
+console.log(path.sep)
+
+const filePath = path.join('/content/', 'subfolder', 'test.txt')
+console.log(filePath)
+
+const base = path.basename(filePath)
+console.log(base)
+
+const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
+console.log(absolute)
