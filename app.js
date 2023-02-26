07b6fac0e28a0869037bb407d62ef49e6ca22040
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-02-26 19:59:27 +0100

    Create a variable named second in app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const { readFileSync, writeFileSync } = require('fs')
 
-const first = readFileSync('./content/first.txt')
+const first = readFileSync('./content/first.txt', 'utf-8')
+const second = readFileSync('./content/second.txt', 'utf-8')
+
+writeFileSync('./content/result-sync.txt', `Here is the result : ${first}, ${second}`, { flag: 'a'})
