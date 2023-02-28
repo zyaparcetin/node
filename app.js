bd9f9aa94bb353f0b09cacaa2c5bdc2574ebbc5b
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-02-28 21:34:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ readFile('./content/first.txt', 'utf8', (err, result) => {
     }
     const second = result
     writeFile('.content/result-async.txt', `Here is the result : ${first}, ${second}`, (err,result) => {
-    if(err){
-      console.log(err);
-      return;    
-    }
-    console.log(result)
+      if(err){
+        console.log(err);
+        return;    
+      }
+      console.log(result)
     })
   })
 })
