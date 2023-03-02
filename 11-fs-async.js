16ca3123a34c8553f8d248d44a5657333c03ed60
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-03-02 19:14:27 +0100

    Add 3,21,25th lines to 11-fs-async.js

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,11 +1,12 @@
 const { readFile, writeFile } = require('fs')
 
+console.log('start')
 readFile('./content/first.txt', 'utf8', (err, result) => {
   if (err) {
     console.log(err)
     return
   }
-  const first = result;
+  const first = result
   readFile('./content/second.txt', 'utf8', (err, result) => {
     if (err) {
       console.log(err)
@@ -14,10 +15,11 @@ readFile('./content/first.txt', 'utf8', (err, result) => {
     const second = result
     writeFile('.content/result-async.txt', `Here is the result : ${first}, ${second}`, (err,result) => {
       if(err){
-        console.log(err);
-        return;    
+        console.log(err)
+        return
       }
-      console.log(result)
+      console.log('done with this task')
     })
   })
 })
+console.log('starting next task')
