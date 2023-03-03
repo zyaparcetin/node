12ce468068f43f7fd5e0da1534086d9bb2c3fbe0
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-03-03 17:15:36 +0100

    Add a comment line in app.js

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ const server = http.createServer((req,res)=> {
     res.end('Welcome to our page')
     return
   }
-  if(req.url === '/about') {
+  else if(req.url === '/about') {
     res.end('Here is our short history')
     return
   }
-  res.end(`
+  else res.end(`
     <h1>Ooops!</h1>
   <p>We can't seem to find the page you are looking for</p>
   <a href="/">back home</a>  
   `)
+  // setup return
+  // if / else if / else 
 })
 
 server.listen(5000)
