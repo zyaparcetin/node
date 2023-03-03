674dc7de80a11ba27328c95ccc0639da402d68f9
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-03-03 16:58:21 +0100

    Add if conditional in app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=> {
-  res.write('Welcome to our page')
-  res.end()
+  if(req.url === '/'){
+    res.end('Welcome to our page')
+  }
+  if(req.url === '/about'){
+    res.end('Here is our short history')
+  }
+  res.end(`
+  <h1>Ooops!</h1>
+  `)
 })
 
 server.listen(5000)
