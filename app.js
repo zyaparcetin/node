c1893c93f8f12e2075a0d6d1e6787f7e8154d24d
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-03-03 17:07:46 +0100

    Fix indentation in app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=> {
-  if(req.url === '/'){
+  if(req.url === '/') {
     res.end('Welcome to our page')
   }
-  if(req.url === '/about'){
+  if(req.url === '/about') {
     res.end('Here is our short history')
   }
   res.end(`
-  <h1>Ooops!</h1>
-<p>We can't seem to find the page you are looking for</p>
-<a href="/">back home</a>  
+    <h1>Ooops!</h1>
+  <p>We can't seem to find the page you are looking for</p>
+  <a href="/">back home</a>  
   `)
 })
 
